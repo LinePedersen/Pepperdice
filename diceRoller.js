14593890b630323120c5845338613965386274a0
mBoegh <boegh.skole@gmail.com>
2023-03-20 16:48:01 +0100

    Largest digit first
    Made it so that the dice with the largest integer is displayed first

--- diceRoller.js
+++ diceRoller.js
@@ -4,8 +4,27 @@ function rollDice() {
   var roll2 = Math.floor(Math.random() * 6) + 1;
 
   // set the image source for each die based on the roll
-  document.getElementById("dice1").src = "dice" + roll1 + ".png";
-  document.getElementById("dice2").src = "dice" + roll2 + ".png";
-}
 
-rollDice()
+  if (roll1 >= roll2){
+    document.getElementById("dice1").src = "dice" + roll1 + ".png";
+    document.getElementById("dice2").src = "dice" + roll2 + ".png";
+    const result = roll1.toString + roll2.toString
+    document.getElementById("resultingThrow").src = result
+  }
+
+  else{
+    document.getElementById("dice1").src = "dice" + roll2 + ".png";
+    document.getElementById("dice2").src = "dice" + roll1 + ".png";
+    const result = roll2.toString + roll1.toString
+    document.getElementById("resultingThrow").src = result
+  }
+
+  return result
+
+}
+let rolls = [];
+
+resultingRoll = rollDice()
+
+rolls.push(resultingRoll)
+
